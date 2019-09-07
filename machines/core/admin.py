56671449b610cb6085ee5e828b2a33f341c48cec
Guilherme Luis <guile.hm@hotmail.com>
2019-09-07 09:27:58 -0300

    create machine admin

--- machines/core/admin.py
+++ machines/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from machines.core.models import Machine
+
+
+@admin.register(Machine)
+class MachineAdmin:
+    list_display = ('code', 'name', 'on_sale', 'price')
+    list_filter = ('code', 'name', 'on_sale')
+    search_fields = ('code', 'name', 'description')
