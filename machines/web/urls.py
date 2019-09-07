be65435b8848dc61034c53feef7e33d76f66c41a
Guilherme Luis <guile.hm@hotmail.com>
2019-09-07 12:28:10 -0300

    create views

--- machines/web/urls.py
+++ machines/web/urls.py
@@ -5,4 +5,7 @@ app_name = 'web'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('machines/<int:machine_id>/', views.machine_detail, name='machine-detail'),
+    path('modules/<int:module_id>/', views.module_detail, name='module-detail'),
+    path('variations/<int:variation_id>/', views.variation_detail, name='variation-detail'),
 ]
--- machines/web/views.py
+++ machines/web/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from machines.product.models import Machine, Module, Variation
 
 
 def index(request):
-    return render(request, 'web/index.html')
+    machines = Machine.objects.all()
+    modules = Module.objects.all()
+    return render(request, 'web/index.html', {
+        'machines': machines,
+        'modules': modules,
+    })
+
+
+def machine_detail(request, machine_id):
+    machine = Machine.objects.get(id=machine_id)
+    return render(request, 'web/machine-detail.html', {
+        'machine': machine,
+    })
+
+
+def module_detail(request, module_id):
+    module = Module.objects.get(id=module_id)
+    return render(request, 'web/module-detail.html', {
+        'module': module,
+    })
+
+
+def variation_detail(request, variation_id):
+    variation = Variation.objects.get(id=variation_id)
+    return render(request, 'web/variation-detail.html', {
+        'variation': variation,
+    })
