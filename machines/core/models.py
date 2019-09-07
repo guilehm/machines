6a089fad7f9773e0ead0b042f053622eb325c845
Guilherme Luis <guile.hm@hotmail.com>
2019-09-07 09:58:23 -0300

    improve models

--- machines/core/models.py
+++ machines/core/models.py
@@ -6,16 +6,20 @@ class Machine(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(null=True, blank=True)
     on_sale = models.BooleanField(default=False, db_index=True)
-    pictures = models.ManyToManyField('core.Picture', related_name='machines')
-    picture_list = models.ForeignKey(
+    pictures = models.ManyToManyField('core.Picture')
+    picture_primary = models.ForeignKey(
         'core.Picture',
         null=True,
         blank=True,
         related_name='machines',
         on_delete=models.CASCADE,
     )
-    modules = models.ManyToManyField('core.Module')
-    price = models.DecimalField(max_digits=9, decimal_places=2)
+    price = models.DecimalField(
+        max_digits=9,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
 
     date_added = models.DateTimeField(auto_now_add=True)
     date_changed = models.DateTimeField(auto_now=True)
@@ -30,7 +34,7 @@ class Module(models.Model):
     description = models.TextField(null=True, blank=True)
 
     pictures = models.ManyToManyField('core.Picture', related_name='modules')
-    picture_list = models.ForeignKey(
+    picture_primary = models.ForeignKey(
         'core.Picture', null=True, blank=True, on_delete=models.CASCADE
     )
 
@@ -63,7 +67,6 @@ class Variation(models.Model):
 
 
 class Picture(models.Model):
-    original_file_name = models.CharField(max_length=1024, null=True, blank=True)
     title = models.CharField(max_length=512, null=True, blank=True)
     image = models.ImageField(upload_to='core/picture/image')
 
