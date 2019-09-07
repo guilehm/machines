0ce0353ce8110c105f25f8022198fe53f48fedca
Guilherme Luis <guile.hm@hotmail.com>
2019-09-07 09:29:05 -0300

    create module admin

--- machines/core/admin.py
+++ machines/core/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from machines.core.models import Machine
+from machines.core.models import Machine, Module
 
 
 @admin.register(Machine)
 class MachineAdmin:
     list_display = ('code', 'name', 'on_sale', 'price')
     list_filter = ('code', 'name', 'on_sale')
+    search_fields = ('code', 'name', 'description')
+
+
+@admin.register(Module)
+class ModuleAdmin:
+    list_display = ('code', 'name')
+    list_filter = ('code', 'name')
     search_fields = ('code', 'name', 'description')
